95323b9a340d363cc9bae1218844030f8e5a4138
Christopher Zhu <christopher.zhu.9@gmail.com>
2019-10-29 20:17:49 -0400

    Init src directory

---
+++ src/cidr-splitter.py
@@ -0,0 +1,15 @@
+import ipaddress
+
+'''
+NOTE: ipaddress.ip_network.overlaps and ipaddress.ip_network.address_exclude
+are pretty much exactly what I'm looking for; but I need to do this manually
+since I want to convert this code to other languages (i.e: Java) which are
+not as fortunate ;(
+'''
+
+def main():
+    print('stub')
+
+
+if __name__ == '__main__':
+    main()
