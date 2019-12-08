af13142b9e43703294175aa96dee4741d26da1c2
Christopher Zhu <christopher.zhu.9@gmail.com>
2019-12-08 18:58:53 -0500

    Tweak manual-impl.py

--- src/manual-impl.py
+++ src/manual-impl.py
@@ -1,22 +1,28 @@
 import ipaddress
 
+# `ipaddress.IPv4Address::packed()` is a more succinct, way to get the
+# binary representation of an IP address, but using the following
+# functions from `socket` instead for forward-backward conversion clarity
 from socket import inet_aton, inet_ntoa
+
 from binascii import hexlify, unhexlify
 
-# this can be accomplished by `sorted(ipaddress.ip_network)` already,
+# this can be accomplished by `sorted(ipaddress.IPv4Network)` already,
 # but this is for the purpose of exercise & translatability
 def sort_cidrs(cidrs):
     packed_ip_cidrs = []
     for cidr in cidrs:
         packed_ip_cidrs.append(
-           (inet_aton(str(cidr[0])), cidr.prefixlen)
+           (inet_aton(str(get_cidr_first_addr(cidr))), cidr.prefixlen)
         )
+    # double sort to use netmask as tie-breaker
+    packed_ip_cidrs = sorted(packed_ip_cidrs, key=lambda x: x[1])
     packed_ip_cidrs = sorted(packed_ip_cidrs, key=lambda x: x[0])
 
     sorted_cidrs = []
     for cidr_pair in packed_ip_cidrs:
         sorted_cidrs.append(
-            ipaddress.ip_network('%s/%d' % (inet_ntoa(cidr_pair[0]), cidr_pair[1]))
+            ipaddress.IPv4Network('%s/%d' % (inet_ntoa(cidr_pair[0]), cidr_pair[1]))
         )
     return sorted_cidrs
 
@@ -27,19 +33,23 @@ def get_cidr_first_addr(cidr):
     # return cidr[0]
     return cidr.network_address
 
+# - in translation, ensure integer types match the IP address type;
+#   IPv4 needs 32-bit unsigned ints, IPv6 needs 128-bit unsigned ints
+#   - need to fix bitmask lengths for this too
 def get_cidr_last_addr(cidr):
-    cidr_low_bytes = get_cidr_first_addr(cidr)
+    cidr_low_bytes = inet_aton(str(get_cidr_first_addr(cidr)))
     #print(inet_ntoa(cidr_low_bytes))
     #print(hexlify(cidr_low_bytes))
 
     # using bitwise addition to add hostmask to first address to find 'high'
     # - using bitwise AND with `0xffffffff` on summation to ensure 32-bit length
     #   - TODO: catch for overflow, and raise an error
-    cidr_hostmask_bytes = inet_aton(str(cidr_cidr.hostmask))
+    cidr_hostmask_bytes = inet_aton(str(cidr.hostmask))
     #print(inet_ntoa(cidr_hostmask_bytes))
     #print(hexlify(cidr_hostmask_bytes))
     
-    cidr_high_bytes = int.from_bytes(cidr_low_bytes, byteorder='big', signed=False) + int.from_bytes(cidr_hostmask_bytes, byteorder='big', signed=False)
+    cidr_high_bytes = int.from_bytes(cidr_low_bytes, byteorder='big', signed=False) + \
+        int.from_bytes(cidr_hostmask_bytes, byteorder='big', signed=False)
     #print(cidr_high_bytes)
     cidr_high_bytes = cidr_high_bytes & 0xFFFFFFFF
     #print(cidr_high_bytes)
@@ -48,8 +58,10 @@ def get_cidr_last_addr(cidr):
     #print(inet_ntoa(cidr_high_bytes))
     #print(hexlify(cidr_high_bytes))
 
-    return ipaddress.ip_address(inet_ntoa(cidr_high_bytes))
+    return ipaddress.IPv4Address(inet_ntoa(cidr_high_bytes))
 
+# - in translation, ensure integer types match the IP address type;
+#   IPv4 needs 32-bit unsigned ints, IPv6 needs 128-bit unsigned ints
 def compare_ips(ip_a, ip_b):
     a = int.from_bytes(inet_aton(str(ip_a)), byteorder='big', signed=False)
     b = int.from_bytes(inet_aton(str(ip_b)), byteorder='big', signed=False)
@@ -65,3 +77,21 @@ def compare_ips(ip_a, ip_b):
 def create_cidrs_from_ip_range(low, high):
     print('[ create_cidrs_from_ip_range ] [ STUB ] (%s, %s)' % (low, high))
     return [(str(low), str(high))]
+
+if __name__ == '__main__':
+    print(sort_cidrs([
+        ipaddress.IPv4Network('10.0.2.0/24'),
+        ipaddress.IPv4Network('10.0.1.0/26'),
+        ipaddress.IPv4Network('10.0.1.64/26'),
+        ipaddress.IPv4Network('10.0.1.0/24'),
+        ipaddress.IPv4Network('10.0.0.0/16')
+    ]))
+
+    print(get_cidr_first_addr(ipaddress.IPv4Network('10.0.4.0/22')))
+    print(get_cidr_last_addr(ipaddress.IPv4Network('10.0.4.0/22')))
+
+    print(compare_ips(ipaddress.IPv4Address('10.0.1.0'), ipaddress.IPv4Address('10.0.2.0')))
+    print(compare_ips(ipaddress.IPv4Address('10.0.2.0'), ipaddress.IPv4Address('10.0.2.0')))
+    print(compare_ips(ipaddress.IPv4Address('10.0.3.0'), ipaddress.IPv4Address('10.0.2.0')))
+
+    print(create_cidrs_from_ip_range('10.0.0.0', '10.0.0.7'))
